c861e9be64a65575caf5bfdcb58d9e71e21f22f1
Tim Bogues <boguest@student.brookfieldps.org>
2017-01-14 08:01:23 -0800

    Added to new DEV Computer.

--- 2015 Competition Robot Code/src/org/usfirst/frc/team1099/robot/subsystems/Elevator.java
+++ 2015 Competition Robot Code/src/org/usfirst/frc/team1099/robot/subsystems/Elevator.java
@@ -3,8 +3,6 @@ package org.usfirst.frc.team1099.robot.subsystems;
 
 import org.usfirst.frc.team1099.robot.RobotMap;
 import org.usfirst.frc.team1099.robot.commands.Elevator.Elevator_Stop;
-
-import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Timer;
